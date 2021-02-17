c27198fdc612d9c94e6df3ce8df0a116f8db6c26
Lillie <LillieRugtveit@gmail.com>
2021-02-17 18:50:50 +0100

    Cleaned up code, and added functionality to cards

--- components/packageCard.tsx
+++ components/packageCard.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router'
 import { Component, ReactPropTypes } from 'react'
 import styles from '../styles/Card.module.css'
+import {useState} from 'react' 
 
 class cardProp 
 {
@@ -12,15 +13,28 @@ class cardProp
 }
 
 const PackageCard = (cardProp: cardProp) => {
+    
+
     return (
         <div className={styles.card}>
             <h1 className={styles.packageName}>{cardProp.packageName}</h1>
             <div className={styles.buttons}>
-                <a className={styles.button}>Details</a>
-                <a className={styles.button}>Github</a>
-                <a className={styles.button}>Download</a>
+                
+                <a className={styles.button}
+                style={{display: cardProp.packageUrl? "block" : "none"}} 
+                href={cardProp.packageUrl}>
+                    Github
+                </a>
+                
+                <a className={styles.button} 
+                style={{display: cardProp.packageDownload? "block" : "none"}}
+                href={cardProp.packageDownload}>
+                    Download
+                </a>
             </div>
-            <div className={styles.packageVersion}>v{cardProp.packageVersion}</div>
+            <div className={styles.packageVersion}>
+                v{cardProp.packageVersion}
+            </div>
         </div>
     )
   }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,49 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Header from '../components/header'
 import PackageCard from '../components/packageCard'
+import {GetStaticProps} from 'next'
 
-export default function Home() {
+export default function Home(
+  {packageData}: {
+    packageData: 
+    {
+      id: string,
+      name: string,
+      version: string,
+      dependency: object,
+      url: string,
+      downloadUrl: string,
+      versions: string[]
+    }[]
+  }) {
   return (
-    <div>
+    <div className={styles.container}>
       <Header/>
-      <PackageCard packageName="beatsaber-hook" packageVersion="0.1.0"/>
+      <div className={styles.packageCards}>
+        {packageData.map(({name, downloadUrl, url, version, dependency, versions}) => {
+          return (<div>
+            <PackageCard packageName={name} packageVersion={version} packageUrl={url} packageDownload={downloadUrl}/>
+           </div>)
+        })}
+      </div>
     </div>
   )
+}
+
+export const getStaticProps: GetStaticProps =  async () => {
+  process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+  let apiResponse :Response = await fetch('http://localhost:5000/api/package');
+  let packageData;
+  if(apiResponse.ok)
+  {
+    let json = await apiResponse.json();
+    //console.log(json);
+    packageData = json;
+  }  
+  
+  return {
+    props: {
+      packageData,
+    },
+  }
 }
